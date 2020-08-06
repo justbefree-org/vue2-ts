3ae1789581dc835053c5865249cbcfe6aa5e270f
Just be free <justbefree@126.com>
2020-08-06 16:06:02 +0800

    Load awesome-bem-scss globally

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,17 @@
  * @Author: Just be free
  * @Date:   2020-07-29 10:45:08
  * @Last Modified by:   Just be free
- * @Last Modified time: 2020-07-29 18:49:53
+ * @Last Modified time: 2020-08-06 15:47:56
  * @E-mail: justbefree@126.com
  */
-
 module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "node_modules/awesome-bem-scss/src/bem.scss";`
+      }
+    }
+  },
   devServer: {
     proxy: {
       "/api": {
