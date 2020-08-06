5ef2e87074711da4c479133ae5a01b1394066033
Just be free <justbefree@126.com>
2020-08-06 16:23:28 +0800

    Inject global scss to each scss file

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,14 @@
  * @Author: Just be free
  * @Date:   2020-07-29 10:45:08
  * @Last Modified by:   Just be free
- * @Last Modified time: 2020-08-06 15:47:56
+ * @Last Modified time: 2020-08-06 16:21:58
  * @E-mail: justbefree@126.com
  */
 module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import "node_modules/awesome-bem-scss/src/bem.scss";`
+        prependData: `@import "node_modules/awesome-bem-scss/src/bem.scss";@import "@/theme/global.scss";`
       }
     }
   },
