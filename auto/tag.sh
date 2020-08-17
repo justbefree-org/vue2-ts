766df1cb455f9f4f19a02e6659588582876096e7
Just be free <justbefree@126.com>
2020-08-17 17:06:12 +0800

    new tag dev20200817170612 published by Just be free

--- auto/tag.sh
+++ auto/tag.sh
@@ -2,7 +2,32 @@
 # @Author: Just be free
 # @Date:   2020-08-17 15:45:37
 # @Last Modified by:   Just be free
-# @Last Modified time: 2020-08-17 17:03:20
+# @Last Modified time: 2020-08-17 17:05:57
+function gitUpdate {
+  git add .
+  git commit -m "new tag $1 published by `gitName`" --no-verify
+  git push
+  if [ $? != 0 ]; then
+    git push origin $2 -u
+  fi
+  git tag "$1" -a -m "$1"
+  git push origin "$1"
+  # git push origin --tags
+  echo "Successfully taged"
+  echo "##########################################"
+
+  echo -e "         \033[32;49;1m$1 $3\033[39;49;0m"
+
+  echo "##########################################"
+}
+function modifyConfigFile {
+  if [[ `getSystem` == "Darwin" ]]; then
+    #statements
+    sed -i "" "s/.*version.*/  version: 'The current version is $1 from `gitBranch` branch, published by `gitName`',/" ./src/config/$2.ts
+  else
+    sed -i "s/.*version.*/\tversion: 'The current version is $1 from `gitBranch` branch, published by `gitName`',/" ./src/config/$2.ts
+  fi
+}
 function getSystem {
   system=`uname`
   echo ${system}
@@ -46,52 +71,11 @@ if [[  "$validEnvirnoment" == "" ]]; then
 fi
 echo "The envirnoment is $envirnoment"
 tag="$envirnoment$(date '+%Y%m%d%H%M%S')"
-# cd ..
 echo "auto tag"
 echo -e "Hello, \033[32;49;1m`gitName`\033[39;49;0m"
 
-function modifyConfigFile {
-  if [[ `getSystem` == "Darwin" ]]; then
-    #statements
-    sed -i "" "s/.*version.*/  version: 'The current version is $1 from `gitBranch` branch, published by `gitName`',/" ./src/config/$2.ts
-  else
-    sed -i "s/.*version.*/\tversion: 'The current version is $1 from `gitBranch` branch, published by `gitName`',/" ./src/config/$2.ts
-  fi
-}
 modifyConfigFile $tag $envirnoment
 
-function gitUpdate {
-  git add .
-  git commit -m "new tag $1 published by `gitName`" --no-verify
-  git push
-  if [ $? != 0 ]; then
-    git push origin $2 -u
-  fi
-  git tag "$1" -a -m "$1"
-  git push origin "$1"
-  # git push origin --tags
-  echo "Successfully taged"
-  echo "##########################################"
-
-  echo -e "         \033[32;49;1m$1 $3\033[39;49;0m"
-
-  echo "##########################################"
-}
 gitUpdate $tag $currentBranch $projectNo
 
-# git add .
-# git commit -m "new tag $tag published by `gitName`" --no-verify
-# git push
-# if [ $? != 0 ]; then
-#   git push origin $currentBranch -u
-# fi
-# git tag "$tag" -a -m "$tag"
-# git push origin "$tag"
-# # git push origin --tags
-# echo "Successfully taged"
-# echo "##########################################"
-
-# echo -e "         \033[32;49;1m$tag $projectNo\033[39;49;0m"
-
-# echo "##########################################"
 
