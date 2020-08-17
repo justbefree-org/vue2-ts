a9e16b4650289783bf0bb6c144a79efb60c0c38c
Just be free <justbefree@126.com>
2020-08-17 17:27:23 +0800

    Update log info

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@
  * @Author: Just be free
  * @Date:   2020-07-22 14:06:41
  * @Last Modified by:   Just be free
- * @Last Modified time: 2020-08-17 15:42:28
+ * @Last Modified time: 2020-08-17 17:26:34
  * @E-mail: justbefree@126.com
  */
 // declare let require: object;
@@ -20,6 +20,7 @@ export const getConfig = (configName: string): string | object => {
   return config[configName] || "";
 };
 export const logInfo = (): void => {
+  whoami();
   console.log(
     `%c ${getAppConfig("VUE_APP_DESCRIPTION")} %c`,
     "background: #EA4949; font-size: 30px; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff",
@@ -32,5 +33,13 @@ const logVersion = (): void => {
     `%c ${getConfig("version")} %c`,
     "background: green; font-size: 20px; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff",
     "background:#007aff ; padding: 1px; border-radius: 0 3px 3px 0; font-size: 20px;  color: #fff"
+  );
+};
+const whoami = (): void => {
+  console.log(
+    "%c 元年云出品 %c 商旅前端组 %c",
+    "background:#35495e ; font-size: 20px; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff",
+    "background:#007aff ; padding: 1px; border-radius: 0 3px 3px 0; font-size: 20px;  color: #fff",
+    "background:transparent"
   );
 };
