0f74034f546448591badf0f587bdf20ed87a5921
Just be free <justbefree@126.com>
2021-05-26 11:36:32 +0800

    Rename folder name

---
+++ src/core/Platform/types/index.d.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Just be free
+ * @Date:   2020-07-22 11:33:05
+ * @Last Modified by:   Just be free
+ * @Last Modified time: 2020-07-23 11:07:41
+ * @E-mail: justbefree@126.com
+ */
+import { Component } from "../../types";
+export interface PlatformConstructorParams {
+  App: Component;
+  id: string;
+}
