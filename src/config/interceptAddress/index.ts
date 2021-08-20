f60bb3db84c718e33fa88a6a403ca8a119774490
Just be free <justbefree@126.com>
2021-08-20 14:29:45 +0800

    删除无用代码

--- src/config/interceptAddress/index.ts
+++ src/config/interceptAddress/index.ts
@@ -1,11 +1,5 @@
 module.exports = {
-  timeoutApi: ["/schedule/getStandardV2", "/schedule/getStandard"],
+  timeoutApi: [],
   cancelApi: [
-    "/travelStandard/getTravelStandard",
-    "/schedule/getOrderParams",
-    "/flight/getFlightList",
-    "/Hotel/BaseDetail",
-    "/Hotel/detail",
-    "/Hotel/Hotels"
   ]
 };
