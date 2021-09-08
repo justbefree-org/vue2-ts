e94badd7ac386dcd5f6c24525331c711053ceea8
Just be free <justbefree@126.com>
2021-09-08 14:10:13 +0800

    feat: 新增上传方法

--- src/core/utils/http.ts
+++ src/core/utils/http.ts
@@ -2,17 +2,18 @@
  * @Author: Just be free
  * @Date:   2020-07-28 15:22:10
  * @Last Modified by:   Just be free
- * @Last Modified time: 2021-08-30 11:52:08
+ * @Last Modified time: 2021-09-08 14:09:08
  * @E-mail: justbefree@126.com
  */
 import axios from "axios";
 import { AnyObject } from "../types";
 import * as qs from "qs";
 import { interceptor } from "@/core/utils/interceptor";
-const formData = (config: AnyObject) => {
+const formData = (config: AnyObject, type?: string) => {
+  const contentType = type || "application/x-www-form-urlencoded;charset=utf-8";
   const instance = axios.create({
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+      "Content-Type": contentType,
       ...config.headers
     }
   });
@@ -34,6 +35,18 @@ const json = (config: AnyObject) => {
 const post = (url: string, params: AnyObject, config = {}): Promise<any> => {
   return formData(config)
     .post(url, qs.stringify(params))
+    .then(res => {
+      if (res.status === 200) {
+        return res.data;
+      }
+    })
+    .catch(e => {
+      console.log(e);
+    });
+};
+const upload = (url: string, params: AnyObject, config = {}): Promise<any> => {
+  return formData(config, "multipart/form-data;charset=utf-8")
+    .post(url, params)
     .then(res => {
       if (res.status === 200) {
         return res.data;
@@ -71,8 +84,8 @@ const postJSON = (
       console.log(e);
     });
 };
-const methods = { post, get, postJSON };
-export type HttpMethodTypes = "get" | "post" | "postJSON";
+const methods = { post, get, postJSON, upload };
+export type HttpMethodTypes = "get" | "post" | "postJSON" | "upload";
 const Http = (args: keyof typeof methods) => {
   return methods[args];
 };
