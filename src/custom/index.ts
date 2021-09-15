5ef2a7e07325bed95507c293604d9ce9a9a0c015
Just be free <justbefree@126.com>
2021-09-15 15:20:38 +0800

    当二开模块是空的时候需要指定返回值为Array<string>

--- src/custom/index.ts
+++ src/custom/index.ts
@@ -2,10 +2,11 @@
  * @Author: Just be free
  * @Date:   2021-03-04 14:51:14
  * @Last Modified by:   Just be free
- * @Last Modified time: 2021-07-30 16:19:29
+ * @Last Modified time: 2021-09-15 15:18:42
  * @E-mail: justbefree@126.com
  */
 
+// const customApps = [] as Array<string>;
 const customApps = ["flight"];
 
 export { customApps };
