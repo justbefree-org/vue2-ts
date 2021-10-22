4f3e73f54519c86db29c8a84e3931e1d51bacc60
Just be free <justbefree@126.com>
2021-10-22 19:00:28 +0800

    增加注册路由守卫的方法

--- src/core/RouterManager/types/index.d.ts
+++ src/core/RouterManager/types/index.d.ts
@@ -2,6 +2,9 @@
  * @Author: Just be free
  * @Date:   2020-07-22 14:38:51
  * @Last Modified by:   Just be free
- * @Last Modified time: 2020-07-23 10:38:56
+ * @Last Modified time: 2021-10-22 17:59:45
  * @E-mail: justbefree@126.com
  */
+// import { NavigationGuard } from "vue-router/types/router";
+export type RouterHooksName = "beforeEach" | "beforeResolve" | "afterEach";
+
