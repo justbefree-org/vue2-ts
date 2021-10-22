3af0114bda771b21726c2b36ef0335b5ff2e609d
Just be free <justbefree@126.com>
2021-10-22 19:00:40 +0800

    路由守卫实例

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import { customApps } from "@/custom";
+import { Route, NavigationGuardNext } from "vue-router/types/router";
 Vue.config.productionTip = false;
 
 const p = new Platform({ id: "#app", App });
+p.registerRouterHooks("beforeEach", (from: Route, to: Route, next: NavigationGuardNext) => {
+  console.log(from, to);
+  next();
+});
 p.install("test");
 p.install(customApps);
 p.startUp();
